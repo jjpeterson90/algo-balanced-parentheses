1057a2b16acbcba2ba3726f7c4ddacd99d8e2bd7
Justin <jjpeterson90@hotmail.com>
2022-05-29 12:14:25 -0500

    complete

--- js/balanceParens.js
+++ js/balanceParens.js
@@ -1,6 +1,31 @@
-balanceParens = (str) =>
-{
+balanceParens = (str) => {
+    parensTracker = [[],[]];
+    str = str.split('');  // parensTracker[0] will act as thestack; parensTracker[1] will remember multiple '(' index locations
+    str.forEach((char, i) => {
+        if (char === '(') {
+            parensTracker[0].push(char);
+            parensTracker[1].push(i);
+        }
+        if (char === ')') {
+            if (parensTracker[0].length === 0) {
+                //console.log(`removing ${char} at index ${i}`)
+                //console.log(`before: ${str}`)
+                str[i] = '';
+                //console.log(`after: ${str}`)
+            } else {
+                parensTracker[0].pop();
+                parensTracker[1].pop();
+            }
+        }
+    })
+    
+    if (parensTracker[1].length > 0) {
+        parensTracker[1].forEach(i => {
+            str[i] = '';
+        })
+    }
 
+    return str.join('')
 }
 
 module.exports = { balanceParens }
